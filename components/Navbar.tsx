2ad416e426a68b4adbe068d3b79c10f68fef98e9
Matan Shaviro <matanshaviro@Matans-MBP-3-2.lan>
2022-08-27 18:12:20 +0300

    Implement Navbar and Footer components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
 export function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Phonex</Link>
+      </p>
+
+      <button type="button" className="cart-icon">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
     </div>
   );
 }
